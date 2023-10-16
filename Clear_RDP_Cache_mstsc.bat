393cc9b1d673314f6242e5e9c800e6f9e61e6975
Vijay Kumbhani <102287173+VSKUMBHANI@users.noreply.github.com>
2023-10-16 12:18:33 +0530

    Add files via upload

---
+++ Clear_RDP_Cache_mstsc.bat
@@ -0,0 +1,4 @@
+reg delete "HKEY_CURRENT_USER\Software\Microsoft\Terminal Server Client\Default" /va /f
+reg delete "HKEY_CURRENT_USER\Software\Microsoft\Terminal Server Client\Servers" /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Terminal Server Client\Servers"
+del /ah %homepath%\documents\default.rdp
